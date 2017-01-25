501a9868a972125ef87c4b04ce7a5aa1db121a17
Alexandre Quentin <A.Quentin@adveez.local>
2017-01-25 18:14:18 +0100

    React using states

--- App.jsx
+++ App.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        
+        this.state = {
+            data: 
+            [
+                {
+                    "id":1,
+                    "name":"Foo",
+                    "age":"20"
+                },
+                
+                {
+                    "id":2,
+                    "name":"Bar",
+                    "age":"30"
+                },
+                
+                {
+                    "id":2,
+                    "name":"Bazz",
+                    "age":"40"
+                }
+            ]
+        }
+    }
+    
     render() {
         return (
             <div>
                 <Header />
-                <Content />
+                <table>
+                    <tbody>
+                        {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
+                    </tbody>
+                </table>
+                
             </div>
         );
     }
@@ -21,13 +53,14 @@ class Header extends React.Component {
     }
 }
 
-class Content extends React.Component {
+class TableRow extends React.Component {
     render() {
         return (
-            <div>
-                <h2>Content</h2>
-                <p>The content text !! ghdv</p>
-            </div>
+            <tr>
+                <td>{this.props.data.id}</td>
+                <td>{this.props.data.name}</td>
+                <td>{this.props.data.age}</td>
+            </tr>
         );
     }
 }
