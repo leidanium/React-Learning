263fe1b0365106438f7ffd3061ea032b58800777
Alexandre Quentin <A.Quentin@adveez.local>
2017-01-26 17:05:11 +0100

    Cahnge data with button

--- App.jsx
+++ App.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
+
 class App extends React.Component {
+    constructor() {
+        super();
+        
+        this.state = {
+            data: []
+        }
+        
+        this.setStateHandler = this.setStateHandler.bind(this);
+    }
+    
+    setStateHandler() {
+        var item = "setState..."
+        var myArray = this.state.data;
+        myArray.push(item)
+        this.setState({data: myArray})
+    }
+    
     render() {
         return (
             <div>
-                <h1>{this.props.headerProp}</h1>
-                <h2>{this.props.contentProp}</h2>
+                <button onClick = {this.setStateHandler}>Set State</button>
+                <h4>State Array: {this.state.data}</h4>
             </div>
         );
     }
