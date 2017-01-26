f11957d37ac3054a2c9adf01dbf88c76d27a8efe
Alexandre Quentin <A.Quentin@adveez.local>
2017-01-26 12:24:59 +0100

    state example

--- App.jsx
+++ App.jsx
@@ -1,75 +1,21 @@
 import React from 'react';
 
-const user = {
-    firstName:'Leid',
-    lastName:'Nora'
-}
-
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-}
-
 class App extends React.Component {
     constructor() {
         super();
         
         this.state = {
-            data: 
-            [
-                {
-                    "id":1,
-                    "name":"Foo",
-                    "age":"20"
-                },
-                
-                {
-                    "id":2,
-                    "name":"Bar",
-                    "age":"30"
-                },
-                
-                {
-                    "id":2,
-                    "name":"Bazz",
-                    "age":"40"
-                }
-            ]
+            header: "Header from state...",
+            "content": "content from state"
         }
     }
     
     render() {
         return (
             <div>
-                <Header />
-                <table>
-                    <tbody>
-                        {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
-                    </tbody>
-                </table>
-                
+                <h1>{this.state.header}</h1>
+                <h2>{this.state.content}</h2>
             </div>
-        );
-    }
-}
-
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Header,  plus un petit test : {formatName(user)}! </h1>
-            </div>
-        );
-    }
-}
-
-class TableRow extends React.Component {
-    render() {
-        return (
-            <tr>
-                <td>{this.props.data.id}</td>
-                <td>{this.props.data.name}</td>
-                <td>{this.props.data.age}</td>
-            </tr>
         );
     }
 }
