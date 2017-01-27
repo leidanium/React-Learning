26c644bc5eba91118f0c3c05bc173ec71fd55cd3
Alexandre Quentin <A.Quentin@adveez.local>
2017-01-27 09:43:33 +0100

    data update from a child prop/states

--- App.jsx
+++ App.jsx
@@ -14,16 +14,27 @@ class App extends React.Component {
     };
     
     updateState() {
-        this.setState({data: 'Data updated'})
+        this.setState({data: 'Data updated from child componant...'})
     }
     
     render() {
         return (
             <div>
-                <button onClick= {this.updateState}>Click</button>
-                <h4>{this.state.data}</h4>
-            </div>);
+                <Content myDataProp = {this.state.data} updateStateProp = {this.updateState} />
+            </div>
+        );
     }
 }
    
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <button onClick = {this.props.updateStateProp}> Click for see</button>
+                <h4>{this.props.myDataProp}</h4>
+            </div>
+        );
+    }
+}
+                    
 export default App;
